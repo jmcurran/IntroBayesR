1c582d588e2f04054f7657e0320931a0980276cc
James Curran <j.curran@auckland.ac.nz>
2016-04-07 15:21:57 +1200

    findRE altered to deal with multiple lines

--- R/findRE.R
+++ R/findRE.R
@@ -1,31 +1,40 @@
-findRE = function(pattern, rng = 1:20, root, dummyRun = TRUE, replace = FALSE){
+findRE = function(pattern, rng = 1:20, root, dummyRun = TRUE, replace = FALSE, collapse = FALSE){
   for(i in rng){
     f = setFileNames(i, root)
     cat(f$fName, "\n")
     
     Lines = readLines(f$fullName)
     
-    if(is.logical(replace) && !replace){
-      i = grep(pattern, Lines, perl = TRUE)
-      cat(paste0(f$fName, "\n"))
-      cat(paste0(i, ": ", Lines[i], "\n"))
-    }else{
-      if(!dummyRun){
-        if(length(grep(pattern, Lines, perl = TRUE)) > 0){
-          writeBackup(f$fName, f$path, dummyRun)
-          Lines1 = gsub(pattern, replace, Lines, perl = TRUE)
-          writeLines(Lines1, f$fullName)
-        }
+    if(!collapse){
+      if(is.logical(replace) && !replace){
+        i = grep(pattern, Lines, perl = TRUE)
+        cat(paste0(f$fName, "\n"))
+        cat(paste0(i, ": ", Lines[i], "\n"))
       }else{
-        if(length(grep(pattern, Lines, perl = TRUE)) > 0){
-          writeBackup(f$fName, f$path, dummyRun)
-          repLines = grep(pattern, Lines, perl = TRUE)
-          for(i in repLines){
-            newLine = gsub(pattern, replace, Lines[i], perl = TRUE)
-            cat(paste0(f$fName, ": ", Lines[i], "\n"))
-            cat(paste0(f$fName, ": ", newLine, "\n"))
+        if(!dummyRun){
+          if(length(grep(pattern, Lines, perl = TRUE)) > 0){
+            writeBackup(f$fName, f$path, dummyRun)
+            Lines1 = gsub(pattern, replace, Lines, perl = TRUE)
+            writeLines(Lines1, f$fullName)
+          }
+        }else{
+          if(length(grep(pattern, Lines, perl = TRUE)) > 0){
+            writeBackup(f$fName, f$path, dummyRun)
+            repLines = grep(pattern, Lines, perl = TRUE)
+            for(i in repLines){
+              newLine = gsub(pattern, replace, Lines[i], perl = TRUE)
+              cat(paste0(f$fName, ": ", Lines[i], "\n"))
+              cat(paste0(f$fName, ": ", newLine, "\n"))
+            }
           }
         }
+      }
+    }else{
+      line = paste(Lines, collapse = "\n")
+      r = gregexpr(pattern, line)
+      
+      if(length(r) > 0){
+        lapply(regmatches(line, r), function(e){cat(paste(substr(e, 0, 50), "\n"))})
       }
     }
   }
